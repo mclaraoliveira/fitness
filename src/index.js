4d1e87a138d905de0121777e9bfc5d8472f699bb
Clara Oliveira <122572905+mclaraoliveira@users.noreply.github.com>
2024-04-15 19:09:41 -0300

    Comecei a criar componentes e páginas

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import reactDOM from "react-dom";
+import App from "./App";
+
+const root = reactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+);
